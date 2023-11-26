15b52256ee62e4584440239a541e167aee219605
Christopher Bannon <ceb6086@psu.edu>
2023-11-25 21:27:22 -0500

    Add project files.

---
+++ Gibbs.h
@@ -0,0 +1,25 @@
+
+// Gibbs.h
+#pragma once
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+#ifndef Gibbs 
+#define Gibbs
+
+
+
+double velocityMag2(vector<double>& Dist1vec, vector<double>& Dist2vec, vector<double>& Dist3vec) {
+
+	double velocityMag2 = 0.0;
+	
+	return velocityMag2;
+}
+
+
+
+#endif
---
+++ README.md
@@ -0,0 +1,7 @@
+OpenGL & The Gang Installation: https://ogldev.org/instructions.html
+GLFW Download: https://www.glfw.org/
+GLEW Download: https://glew.sourceforge.net/index.html
+
+Tutorial:
+- http://www.opengl-tutorial.org/
+- https://ogldev.org/
