dbe8013f5988771a42e54121eacd24c92411304a
Christopher Bannon <ceb6086@psu.edu>
2023-11-26 00:56:28 -0500

    kk

--- Gibbs.h
+++ Gibbs.h
@@ -11,6 +11,13 @@ using namespace std;
 #ifndef Gibbs 
 #define Gibbs
 
+double dot(vector<double>& a, vector<double>& b) {
+	double c = 0.0;
+	for (size_t i = 0; i < 3; i++) {
+		c += a[i] * b[i];
+	}
+		return c;
+}
 
 
 double velocityMag2(vector<double>& Dist1vec, vector<double>& Dist2vec, vector<double>& Dist3vec) {
